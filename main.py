85a2746f62af371d7b2e26b791ea5e0b30d39de4
SShivendu <sshivendusharan17092001@gmail.com>
2023-11-25 14:51:47 +0530

    final

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from utils.validateRequest import validateRequest
 from utils.model import Model
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/predict",methods=["POST"])
 @validateRequest()
